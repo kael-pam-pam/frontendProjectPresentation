4190cc486c03196294f1ee4d1abb2f7f796c85f9
kael-pam-pam <59134677+kael-pam-pam@users.noreply.github.com>
2020-10-03 09:05:24 +0300

    добавил задачи в виде комментариев

--- ts/functions.ts
+++ ts/functions.ts
@@ -74,4 +74,40 @@ function deleteSlide(prog:Programm): Programm {
     }
 }
 
-//TODO: функция перемещения 
+//то что выяснили в пятницу в Zoom'е
+//TODO: Programm.selectedSlide это массив слайдов
+//TODO: в массиве Programm.selectedSlide последний слайд отображается на экране (АКТИВНЫЙ СЛАЙД)
+//TODO: функция перемещения selectedSlide внутри массива слайдов
+
+//TODO: Programm.selectedElement это массив элементов
+//TODO: у массива Programm.selectedElement (если там несколько элементов) можно менять ТОЛЬКО положение и масштабировать и удалять
+//TODO: Programm.selectedElement может быть null
+
+//TODO: внести поправки в функцию deleteSlide исходя из того, что Programm.selectedSlide это массив
+//TODO: currentPresentation.slides может быть null, если все слайды удалили
+//TODO: после удаления слайда активным слайдом становится [шедший за ним], или если удаленный слайд был последний - [предыдущий]
+
+//функции Programm
+//TODO: exportPDF()
+//TODO: setCurrentSlide()
+//TODO: saveProject()
+//TODO: loadProject()
+//функции Presentation
+//TODO: changeTitle()
+//функции Slide
+//TODO: setBackground()
+//TODO: setIsSkip()
+//функции элементов
+//TODO: addElement()
+//TODO: deleteSelectedElement()
+//TODO: changePositionSelectedElement()
+//TODO: changeHeightAndWidthSelectedElement()
+//TODO: changeTextObj()
+//TODO: changeShapeObj()
+//TODO: changeTriangle()
+//TODO: changeRect()
+//TODO: changeCircle()
+//функции ArchiveOfState
+//TODO: goBack()
+//TODO: goForward()
+//TODO: saveToArchive()
